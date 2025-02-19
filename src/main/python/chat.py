cf076d36418d11642be0265eba8fbfcf3123b9b8
Screamadelica <1621456391@sjtu.edu.cn>
2025-02-19 16:39:57 +0800

    Update log information and delete log from git

--- src/main/python/chat.py
+++ src/main/python/chat.py
@@ -33,6 +33,8 @@ class ChatServer:
             f.write(f"Log started at {current_time}\n")
             
     def write_log(self, message):
+        current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        message = f"(LOG) {current_time}:\n{message}"
         with open(self.log_path, "a") as f:
             f.write(message)
             f.write("\n")
